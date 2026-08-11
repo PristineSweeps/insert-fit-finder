986a948f6f53f23e2cbf2fc8eb6396d519ce33c8
aajwoodward <aajwoodward@gmail.com>
2026-08-11 16:01:04 -0700

    More Updates
    design changes, additional measurements, additng hearth extension and lintel thickness

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Pristine Sweeps Insert Fit Finder - offline cache.
 // Bump CACHE when index.html changes so phones pick up the new version.
-const CACHE = "fitfinder-v6";
+const CACHE = "fitfinder-v10";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
